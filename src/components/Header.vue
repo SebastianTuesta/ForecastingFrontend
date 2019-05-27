06743b765f4ec116017152f36e1947aa9cd0413e
Sebastian Tuesta <tuestagabo@gmail.com>
2019-05-27 04:21:53 -0500

    Finish Login

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span class="font-weight-light">FINANCIAL FORECASTING APP</span>
+      </v-toolbar-title>
+    </v-toolbar>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import './plugins/axios'
+import './plugins/vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
